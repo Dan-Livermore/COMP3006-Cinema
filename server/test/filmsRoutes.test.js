a403a526fbbd4d068f6ffc9ef9dff041e1e40299
Dan Livermore <danieljlivermore@gmail.com>
2024-01-12 20:32:12 +0000

    EC Updated timeout and added delete to all tests that create new entites

--- server/test/filmsRoutes.test.js
+++ server/test/filmsRoutes.test.js
@@ -1,227 +1,292 @@
-// import { expect} from "chai";
-// import supertest from "supertest";
-// import app from "../index.js";
+import { expect } from "chai";
+import supertest from "supertest";
+import app from "../index.js";
 
-// const request = supertest(app);
+const request = supertest(app);
 
-// describe("Films Routes", () => {
-//   let server;
-//   it("Get is valid request", () => {
-//     return request
-//       .get("/films")
-//       .expect(200)
-//       .then((res) => {
-//         expect(res.body).to.be.an("object");
-//       });
-//   });
-//   it("Get all Films", () => {
-//     return request
-//       .get("/films")
-//       .expect(200)
-//       .expect("Content-Type", /json/)
-//       .then((res) => {
-//         expect(res.body).to.be.an("object");
-//       });
-//   });
-//   it("Get One - ID = Blade Runner 2049", () => {
-//     return request
-//     .get("/films/6583347eed59946e06f53065")
-//     .expect(200)
-//     .expect("Content-Type", /json/)
-//     .then((res) => {
-//       expect(res.body).to.be.an("object");
-
-//       // Check releaseDate to see if it is a real date
-//       expect(res.body.releaseDate).to.be.a('string');
-//       expect(() => new Date(res.body.releaseDate)).to.not.throw();
-
-//       // Check runtime is an int
-//       expect(res.body.runtime).to.be.a('number');
-//       expect(res.body.runtime % 1).to.equal(0);
-
-//       // Check other fields are string
-//       expect(res.body.title).to.be.a('string');
-//       expect(res.body.director).to.be.a('string');
-//       expect(res.body.ageRating).to.be.a('string');
-//       expect(res.body.description).to.be.a('string');
-//       expect(res.body.poster).to.be.a('string');
-//     });
-// });
-// it("Get One - ID Too Short", () => {
-//   return request
-//   .get("/films/1")
-//   .expect(500)
-//   .expect("Content-Type", /json/)
-//   .then((res) => {
-//     expect(res.body).to.be.an("object");
-//   });
-// });
-// it("Get One - No ID = Return All", () => {
-//   return request
-//   .get("/films/")
-//   .expect(200) 
-//   .expect("Content-Type", /json/)
-//   .then((res) => {
-//     expect(res.body).to.be.an("object");
-//   });
-// });
-// it("Get One - No ID in DB", () => {
-//   return request
-//   .get("/films/111111111111111111111111")
-//   .expect(200)
-//   .expect("Content-Type", /json/)
-//   .then((res) => {
-//     expect(res.body).to.be.an("null");
-//   });
-// });
+describe("Films Routes", () => {
+  let server;
   
+  it("CREATE is a valid request", () => {
+    const newFilmData = {
+      title: "New Film",
+      director: "New Director",
+      releaseDate: "2023-01-01T00:00:00.000Z",
+      ageRating: "New Rating",
+      runtime: 100,
+      description: "New Rating",
+      poster: "New Image Link",
+    };
 
-// it("Create Film", () => {
-//   const newFilmData = {
-//     title: "New Film",
-//     director: "New Director",
-//     releaseDate: "2023-01-01T00:00:00.000Z",
-//     ageRating: "New Rating",
-//     runtime: 100,
-//     description: "New Rating",
-//     poster: "New Image Link",
-//   };
+    return request
+      .post("/films")
+      .send(newFilmData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+        expect(res.body.title).to.equal(newFilmData.title);
+        expect(res.body.director).to.equal(newFilmData.director);
+        expect(res.body.releaseDate).to.equal(newFilmData.releaseDate);
+        expect(res.body.ageRating).to.equal(newFilmData.ageRating);
+        expect(res.body.runtime).to.equal(newFilmData.runtime);
+        expect(res.body.description).to.equal(newFilmData.description);
+        expect(res.body.poster).to.equal(newFilmData.poster);
+        return request.delete(`/films/${res.body._id}`).expect(200);
+      });
+  });
 
-//   return request
-//     .post("/films")
-//     .send(newFilmData)
-//     .expect(201)  
-//     .expect("Content-Type", /json/)
-//     .then((res) => {
-//       expect(res.body).to.be.an('object');
-//       expect(res.body.title).to.equal(newFilmData.title);
-//       expect(res.body.director).to.equal(newFilmData.director);
-//       expect(res.body.releaseDate).to.equal(newFilmData.releaseDate);
-//       expect(res.body.ageRating).to.equal(newFilmData.ageRating);
-//       expect(res.body.runtime).to.equal(newFilmData.runtime);
-//       expect(res.body.description).to.equal(newFilmData.description);
-//       expect(res.body.poster).to.equal(newFilmData.poster);
-//     });
-// });
-// it("Create Film - releaseDate not a date", () => {
-//   const newFilmData = {
-//     title: "New Film",
-//     director: "New Director",
-//     releaseDate: "New Date",
-//     ageRating: "New Rating",
-//     runtime: 100,
-//     description: "New Rating",
-//     poster: "New Image Link",
-//   };
-//   return request
-//     .post("/films")
-//     .send(newFilmData)
-//     .expect(500)  
-//     .expect("Content-Type", /json/)
-// });
-// });
+  it("READ is valid request", () => {
+    return request
+      .get("/films")
+      .expect(200)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+      });
+  });
 
-// it("Create Film - runtime is a string", () => {
-//   const newFilmData = {
-//     title: "New Film",
-//     director: "New Director",
-//     releaseDate: "2023-01-01T00:00:00.000Z",
-//     ageRating: "New Rating",
-//     runtime: "Two hours", 
-//     description: "New Description",
-//     poster: "New Image Link",
-//   };
-//   return request
-//     .post("/films")
-//     .send(newFilmData)
-//     .expect(500)  
-//     .expect("Content-Type", /json/)
-//     .then((res) => {
-//       expect(res.body).to.be.an('object');
-//     });
-// });
+  it("UPDATE is a valid request", () => {
+    const newFilmData = {
+      title: "New Film",
+      director: "New Director",
+      releaseDate: "2023-01-01T00:00:00.000Z",
+      ageRating: "New Rating",
+      runtime: 100,
+      description: "New Description",
+      poster: "New Image Link",
+    };
+    const updatedFilmData = {
+      title: "Updated Film",
+      director: "Updated Director",
+      releaseDate: "2023-02-01T00:00:00.000Z",
+      ageRating: "Updated Rating",
+      runtime: 120,
+      description: "Updated Description",
+      poster: "Updated Image Link",
+    };
+    return request
+      .post("/films")
+      .send(newFilmData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        return request
+          .put(`/films/${res.body._id}`)
+          .send(updatedFilmData)
+          .expect(200)
+          .expect("Content-Type", /json/)
+          .then((updatedRes) => {
+            expect(updatedRes.body).to.be.an("object");
+            return request.delete(`/films/${res.body._id}`).expect(200);
+          });
+      });
+  });
 
-// it("Delete an existing film", () => {
-//   const newFilmData = {
-//     title: "New Film",
-//     director: "New Director",
-//     releaseDate: "2023-01-01T00:00:00.000Z",
-//     ageRating: "New Rating",
-//     runtime: 100,
-//     description: "Description",
-//     poster: "New Image Link",
-//   };
+  
+  it("DELETE is a valid request", () => {
+    const newFilmData = {
+      title: "New Film",
+      director: "New Director",
+      releaseDate: "2023-01-01T00:00:00.000Z",
+      ageRating: "New Rating",
+      runtime: 100,
+      description: "Description",
+      poster: "New Image Link",
+    };
 
-//   return request
-//     .post("/films")
-//     .send(newFilmData)
-//     .expect(201)
-//     .expect("Content-Type", /json/)
-//     .then((createRes) => {
-//       return request
-//         .delete(`/films/${createRes.body._id}`)
-//         .expect(200); 
-//     });
-// });
+    return request
+      .post("/films")
+      .send(newFilmData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((createRes) => {
+        return request.delete(`/films/${createRes.body._id}`).expect(200);
+      });
+  });
 
-// let filmId;
+  it("GET ALL Films", () => {
+    return request
+      .get("/films")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+      });
+  });
+  it("GET ONE - ID = Blade Runner 2049", () => {
+    return request
+      .get("/films/6583347eed59946e06f53065")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
 
-// before((done) => {
-//   const newFilmData = {
-//     title: 'New Film',
-//     director: 'New Director',
-//     releaseDate: '2023-01-01T00:00:00.000Z',
-//     ageRating: 'New Rating',
-//     runtime: 100,
-//     description: 'New Description',
-//     poster: 'New Image Link',
-//   };
+        // Check releaseDate to see if it is a real date
+        expect(res.body.releaseDate).to.be.a("string");
+        expect(() => new Date(res.body.releaseDate)).to.not.throw();
 
-//   request
-//     .post('/films')
-//     .send(newFilmData)
-//     .expect(201)
-//     .expect('Content-Type', /json/)
-//     .end((err, res) => {
-//       if (err) return done(err);
-//       filmId = res.body._id;
-//       done();
-//     });
-// });
+        // Check runtime is an int
+        expect(res.body.runtime).to.be.a("number");
+        expect(res.body.runtime % 1).to.equal(0);
 
-// it('Update Film', (done) => {
-//   const updatedFilmData = {
-//     title: 'Updated Film',
-//     director: 'Updated Director',
-//     releaseDate: '2023-02-01T00:00:00.000Z',
-//     ageRating: 'Updated Rating',
-//     runtime: 120,
-//     description: 'Updated Description',
-//     poster: 'Updated Image Link',
-//   };
+        // Check other fields are string
+        expect(res.body.title).to.be.a("string");
+        expect(res.body.director).to.be.a("string");
+        expect(res.body.ageRating).to.be.a("string");
+        expect(res.body.description).to.be.a("string");
+        expect(res.body.poster).to.be.a("string");
+      });
+  });
+  it("GET ONE - ID Too Short", () => {
+    return request
+      .get("/films/1")
+      .expect(500)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+      });
+  });
+  it("GET ONE - No ID = Return All", () => {
+    return request
+      .get("/films/")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+      });
+  });
+  it("GET ONE - No ID in DB", () => {
+    return request
+      .get("/films/111111111111111111111111")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("null");
+      });
+  });
 
-//   request
-//     .put(`/films/${filmId}`)
-//     .send(updatedFilmData)
-//     .expect(200)
-//     .expect('Content-Type', /json/)
-//     .end(() => {
-//       request
-//         .get(`/films/${filmId}`)
-//         .expect(200)
-//         .expect('Content-Type', /json/)
-//         .expect((getRes) => {
-//           expect(getRes.body.title).to.equal(updatedFilmData.title);
-//           expect(getRes.body.director).to.equal(updatedFilmData.director);
-//           expect(getRes.body.releaseDate).to.equal(updatedFilmData.releaseDate);
-//           expect(getRes.body.ageRating).to.equal(updatedFilmData.ageRating);
-//           expect(getRes.body.runtime).to.equal(updatedFilmData.runtime);
-//           expect(getRes.body.description).to.equal(updatedFilmData.description);
-//           expect(getRes.body.poster).to.equal(updatedFilmData.poster);
-//         })
-//         .end((getErr) => {
-//           if (getErr) return done(getErr);
-//           done(); 
-//         });
-//     });
-// });
+  it("CREATE Film", () => {
+    const newFilmData = {
+      title: "New Film",
+      director: "New Director",
+      releaseDate: "2023-01-01T00:00:00.000Z",
+      ageRating: "New Rating",
+      runtime: 100,
+      description: "New Rating",
+      poster: "New Image Link",
+    };
+
+    return request
+      .post("/films")
+      .send(newFilmData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+        expect(res.body.title).to.equal(newFilmData.title);
+        expect(res.body.director).to.equal(newFilmData.director);
+        expect(res.body.releaseDate).to.equal(newFilmData.releaseDate);
+        expect(res.body.ageRating).to.equal(newFilmData.ageRating);
+        expect(res.body.runtime).to.equal(newFilmData.runtime);
+        expect(res.body.description).to.equal(newFilmData.description);
+        expect(res.body.poster).to.equal(newFilmData.poster);
+        return request.delete(`/films/${res.body._id}`).expect(200);
+      });
+  });
+  it("CREATE Film - releaseDate not a date", () => {
+    const newFilmData = {
+      title: "New Film",
+      director: "New Director",
+      releaseDate: "New Date",
+      ageRating: "New Rating",
+      runtime: 100,
+      description: "New Rating",
+      poster: "New Image Link",
+    };
+    return request
+      .post("/films")
+      .send(newFilmData)
+      .expect(500)
+      .expect("Content-Type", /json/);
+  });
+
+  it("CREATE Film - runtime is a string", () => {
+    const newFilmData = {
+      title: "New Film",
+      director: "New Director",
+      releaseDate: "2023-01-01T00:00:00.000Z",
+      ageRating: "New Rating",
+      runtime: "Two hours",
+      description: "New Description",
+      poster: "New Image Link",
+    };
+    return request
+      .post("/films")
+      .send(newFilmData)
+      .expect(500)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+      });
+  });
+
+  it("DELETE an existing film", () => {
+    const newFilmData = {
+      title: "New Film",
+      director: "New Director",
+      releaseDate: "2023-01-01T00:00:00.000Z",
+      ageRating: "New Rating",
+      runtime: 100,
+      description: "Description",
+      poster: "New Image Link",
+    };
+
+    return request
+      .post("/films")
+      .send(newFilmData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((createRes) => {
+        return request.delete(`/films/${createRes.body._id}`).expect(200);
+      });
+  });
+
+ 
+
+  it("UPDATE a Film", () => {
+    const newFilmData = {
+      title: "New Film",
+      director: "New Director",
+      releaseDate: "2023-01-01T00:00:00.000Z",
+      ageRating: "New Rating",
+      runtime: 100,
+      description: "New Description",
+      poster: "New Image Link",
+    };
+    const updatedFilmData = {
+      title: "Updated Film",
+      director: "Updated Director",
+      releaseDate: "2023-02-01T00:00:00.000Z",
+      ageRating: "Updated Rating",
+      runtime: 120,
+      description: "Updated Description",
+      poster: "Updated Image Link",
+    };
+    return request
+      .post("/films")
+      .send(newFilmData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        return request
+          .put(`/films/${res.body._id}`)
+          .send(updatedFilmData)
+          .expect(200)
+          .expect("Content-Type", /json/)
+          .then((updatedRes) => {
+            expect(updatedRes.body).to.be.an("object");
+            return request.delete(`/films/${res.body._id}`).expect(200);
+          });
+      });
+  });
+});
--- server/test/showingsRoutes.test.js
+++ server/test/showingsRoutes.test.js
@@ -6,25 +6,7 @@ const request = supertest(app);
 
 describe("Showings Routes", () => {
   let server;
-  it("GET is a valid request", () => {
-    return request
-      .get("/showings")
-      .expect(200)
-      .expect("Content-Type", /json/)
-      .then((res) => {
-        expect(res.body).to.be.an("object");
-      });
-  });
-  it("GET all Showings", () => {
-    return request
-      .get("/showings")
-      .expect(200)
-      .expect("Content-Type", /json/)
-      .then((res) => {
-        expect(res.body).to.be.an("object");
-      });
-  });
-
+  
   it("CREATE is a valid request", () => {
     const newShowingData = {
       filmID: "000000000000000000000000",
@@ -48,6 +30,91 @@ describe("Showings Routes", () => {
         .expect(200);
       });
   });
+
+  it("READ is a valid request", () => {
+    return request
+      .get("/showings")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+      });
+  });
+  
+  it("UPDATE is a valid request", () => {
+    const newShowingData = {
+      filmID: "000000000000000000000000",
+      startTime: "2023-01-01T00:00:00.000Z",
+      row1: "Unit Test",
+      row2: "Unit Test",
+      row3: "Unit Test",
+      row4: "Unit Test",
+      totalSeats: 100,
+      seatsRemaining: 100,
+    };
+    const updatedData = {
+      filmID: "111111111111111111111111",
+      startTime: "2023-01-01T00:00:00.000Z",
+      row1: "Unit Test1",
+      row2: "Unit Test2",
+      row3: "Unit Test3",
+      row4: "Unit Test4",
+      totalSeats: 100,
+      seatsRemaining: 100,
+    };
+
+    return request
+      .post("/showings")
+      .send(newShowingData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        return request
+          .put(`/showings/${res.body._id}`)
+          .send(updatedData)
+          .expect(200)
+          .expect("Content-Type", /json/)
+          .then((updatedRes) => {
+            expect(updatedRes.body).to.be.an("object");
+            return request
+              .delete(`/showings/${res.body._id}`)
+              .expect(200);
+          });
+      });
+  });
+
+  it("DELETE is a valid request", () => {
+    const newShowingData = {
+      filmID: "000000000000000000000000",
+      startTime: "2023-01-01T00:00:00.000Z",
+      row1: "Unit Test",
+      row2: "Unit Test",
+      row3: "Unit Test",
+      row4: "Unit Test",
+      totalSeats: 100,
+      seatsRemaining: 100,
+    };
+
+    return request
+      .post("/showings")
+      .send(newShowingData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((createRes) => {
+        return request.delete(`/showings/${createRes.body._id}`).expect(200);
+      });
+  });
+
+  it("GET ALL Showings", () => {
+    return request
+      .get("/showings")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("object");
+      });
+  });
+
   
   it("CREATE a Showing", () => {
     const newShowingData = {
@@ -119,91 +186,8 @@ describe("Showings Routes", () => {
       .expect("Content-Type", /json/);
   });
 
-  it("DELETE is a valid request", () => {
-    const newShowingData = {
-      filmID: "000000000000000000000000",
-      startTime: "2023-01-01T00:00:00.000Z",
-      row1: "Unit Test",
-      row2: "Unit Test",
-      row3: "Unit Test",
-      row4: "Unit Test",
-      totalSeats: 100,
-      seatsRemaining: 100,
-    };
+  
 
-    return request
-      .post("/showings")
-      .send(newShowingData)
-      .expect(201)
-      .expect("Content-Type", /json/)
-      .then((createRes) => {
-        return request.delete(`/showings/${createRes.body._id}`).expect(200);
-      });
-  });
-
-  it("DELETE a Showing", () => {
-    const newShowingData = {
-      filmID: "000000000000000000000000",
-      startTime: "2023-01-01T00:00:00.000Z",
-      row1: "Unit Test",
-      row2: "Unit Test",
-      row3: "Unit Test",
-      row4: "Unit Test",
-      totalSeats: 100,
-      seatsRemaining: 100,
-    };
-
-    return request
-      .post("/showings")
-      .send(newShowingData)
-      .expect(201)
-      .expect("Content-Type", /json/)
-      .then((createRes) => {
-        return request.delete(`/showings/${createRes.body._id}`).expect(200);
-      });
-  });
-
-  it("UPDATE is a valid request", () => {
-    const newShowingData = {
-      filmID: "000000000000000000000000",
-      startTime: "2023-01-01T00:00:00.000Z",
-      row1: "Unit Test",
-      row2: "Unit Test",
-      row3: "Unit Test",
-      row4: "Unit Test",
-      totalSeats: 100,
-      seatsRemaining: 100,
-    };
-    const updatedData = {
-      filmID: "111111111111111111111111",
-      startTime: "2023-01-01T00:00:00.000Z",
-      row1: "Unit Test1",
-      row2: "Unit Test2",
-      row3: "Unit Test3",
-      row4: "Unit Test4",
-      totalSeats: 100,
-      seatsRemaining: 100,
-    };
-
-    return request
-      .post("/showings")
-      .send(newShowingData)
-      .expect(201)
-      .expect("Content-Type", /json/)
-      .then((res) => {
-        return request
-          .put(`/showings/${res.body._id}`)
-          .send(updatedData)
-          .expect(200)
-          .expect("Content-Type", /json/)
-          .then((updatedRes) => {
-            expect(updatedRes.body).to.be.an("object");
-            return request
-              .delete(`/showings/${res.body._id}`)
-              .expect(200);
-          });
-      });
-  });
 
   it("UPDATE a Showing", () => {
     const newShowingData = {
@@ -244,6 +228,30 @@ describe("Showings Routes", () => {
               .delete(`/showings/${res.body._id}`)
               .expect(200);
           });
+      });
+  });
+
+  
+
+  it("DELETE a Showing", () => {
+    const newShowingData = {
+      filmID: "000000000000000000000000",
+      startTime: "2023-01-01T00:00:00.000Z",
+      row1: "Unit Test",
+      row2: "Unit Test",
+      row3: "Unit Test",
+      row4: "Unit Test",
+      totalSeats: 100,
+      seatsRemaining: 100,
+    };
+
+    return request
+      .post("/showings")
+      .send(newShowingData)
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((createRes) => {
+        return request.delete(`/showings/${createRes.body._id}`).expect(200);
       });
   });
 });
